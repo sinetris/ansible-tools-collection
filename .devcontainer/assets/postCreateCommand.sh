a2428d5e52fbca5acee379d93b494c2a97011c57
Duilio Ruggiero <duilio@sinetris.info>
2025-09-12 09:47:56 +0000

    Fix DOCKER_HOST env variable in devcontainer

--- .devcontainer/assets/postCreateCommand.sh
+++ .devcontainer/assets/postCreateCommand.sh
@@ -9,3 +9,11 @@ cp "${PROJECT_WORKDIR}/.devcontainer/assets/.gitignore_global" ~/.gitignore_glob
 cd "${PROJECT_WORKDIR}"
 pre-commit install --install-hooks
 ansible-galaxy install -r requirements.yml
+
+# Ensure proper DOCKER_HOST for docker-in-docker
+# for bash
+sed -zi '/export DOCKER_HOST=/!s/$/\nexport DOCKER_HOST=unix:\/\/\/var\/run\/docker\.sock/' \
+  ~/.bashrc
+# for zsh
+sed -zi '/export DOCKER_HOST=/!s/$/\nexport DOCKER_HOST=unix:\/\/\/var\/run\/docker\.sock/' \
+  ~/.zshenv
