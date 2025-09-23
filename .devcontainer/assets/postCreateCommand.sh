930941afe12ab348b98e64e3d6899d2eea3297da
Duilio Ruggiero <duilio@sinetris.info>
2025-09-23 12:08:07 +0000

    Cleanup global gitignore

--- .devcontainer/assets/postCreateCommand.sh
+++ .devcontainer/assets/postCreateCommand.sh
@@ -4,6 +4,8 @@ set -Eeuo pipefail
 # Is likely that your host UID and the container UID are different.
 # To avoid complaints when running 'tox', we tell git to ignore it.
 git config --global --add safe.directory "${PROJECT_WORKDIR:?}"
+
+# Use a global gitignore to ignore common artifacts
 cp "${PROJECT_WORKDIR}/.devcontainer/assets/.gitignore_global" ~/.gitignore_global
 
 # Ensure proper DOCKER_HOST for docker-in-docker (for bash and zsh)
